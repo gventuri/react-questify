0749d39b0e0f45f45d3f5fe0ec8b0970729accf0
Gabriele <lele.venturi@gmail.com>
2019-05-01 11:52:12 +0200

    Updates

---
+++ precache-manifest.ba64bbd98320088d248c5fe45cc22aec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf2d92ec9d739fc8c833341473f4fcc3",
+    "url": "/react-questify/index.html"
+  },
+  {
+    "revision": "c942ac7e5e4fa21204cd",
+    "url": "/react-questify/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "7e584974d17ee8dcd31c",
+    "url": "/react-questify/static/js/2.cc16dd21.chunk.js"
+  },
+  {
+    "revision": "c942ac7e5e4fa21204cd",
+    "url": "/react-questify/static/js/main.d171f1e7.chunk.js"
+  },
+  {
+    "revision": "a3871a17830c03767bf7",
+    "url": "/react-questify/static/js/runtime~main.36e20834.js"
+  }
+]);
